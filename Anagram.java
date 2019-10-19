22c06f3c6bc1237d8b6b77a5ff25b039116387c5
git-chub <bien_chris@yahoo.com>
2019-10-19 13:50:11 -0500

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -20,7 +20,7 @@ public class Anagram {
     }
 
     public static void isAnagram(String input1, String input2) {
-        //Remove all whitespace first
+        
         String s1 = input1.replaceAll("\\s", "");
 
         String s2 = input2.replaceAll("\\s", "");
@@ -30,19 +30,14 @@ public class Anagram {
         if (s1.length() != s2.length()) {
             status = false;
         } else {
-            //Convert into character array
 
             char[] s1Array = s1.toLowerCase().toCharArray();
 
             char[] s2Array = s2.toLowerCase().toCharArray();
 
-            //Sorting both character array
-
             Arrays.sort(s1Array);
 
             Arrays.sort(s2Array);
-
-            //Check if both arrays are equal
 
             status = Arrays.equals(s1Array, s2Array);
         }
